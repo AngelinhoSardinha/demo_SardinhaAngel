847e3a9d114dc3ccefcbc9801075a3c4e7a43633
angel.sardinha.7e8 <angel.sardinha.7e8@itb.cat>
2025-05-04 19:26:05 +0200

    HOLA, BASE DE DATOS DIFERENTE

--- src/main/java/com/example/demo/persistence/repository/IPersonaRepository.java
+++ src/main/java/com/example/demo/persistence/repository/IPersonaRepository.java
@@ -3,5 +3,5 @@ package com.example.demo.persistence.repository;
 import com.example.demo.persistence.entity.PersonaEntity;
 import org.springframework.data.repository.CrudRepository;
 
-public interface IUserRepository extends CrudRepository<PersonaEntity, Long> {
+public interface IPersonaRepository extends CrudRepository<PersonaEntity, Long> {
 }
